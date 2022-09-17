f6ca47ef32e08c992c3089159ffd2afb7216472a
Flavio <victorflavio74@gmail.com>
2022-09-16 22:41:35 -0300

    add articles cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
+import Card from "../components/Card/Card";
 import Footer from "../components/Footer/Footer";
 import Topbar from "../components/Topbar/Topbar";
 
+const artigos: Object[] = [
+  {title: 'Musculação', subtitle: 'É a melhor coisa que há'},
+  {title: 'Musculação', subtitle: 'É a melhor coisa que há'},
+  {title: 'Musculação', subtitle: 'É a melhor coisa que há'},
+  {title: 'Musculação', subtitle: 'É a melhor coisa que há'}
+]
 
 export default function Home(){
   return(
     <div>
       <Topbar />
-      <div>
-        <article>
-          <h1>musculação é top</h1>
-        </article>
-      </div>
+      <Card 
+        articles={artigos.length > 0 ? artigos : undefined}
+        />
       <Footer />
     </div>
   )
